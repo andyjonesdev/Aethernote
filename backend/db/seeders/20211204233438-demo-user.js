e92ccea68f7aca434206ece4b27823c095e63bc3
andyrose507 <drew.jones1998@gmail.com>
2021-12-04 18:00:28 -0700

    login auth works

---
+++ backend/db/seeders/20211204233438-demo-user.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const faker = require('faker');
+const bcrypt = require('bcryptjs');
+
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.bulkInsert('Users', [
+      {
+        email: 'demo@user.io',
+        username: 'Demo-duck',
+        hashedPassword: bcrypt.hashSync('password')
+      }
+    ], {});
+  },
+
+  down: (queryInterface, Sequelize) => {
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete('Users', {
+      username: { [Op.in]: ['Demo-duck'] }
+    }, {});
+  }
+};
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -1,7 +1,45 @@
 const router = require('express').Router();
+const asyncHandler = require('express-async-handler');
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+const { restoreUser, requireAuth } = require('../../utils/auth.js');
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
 
 router.post('/test', function(req, res) {
       res.json({ requestBody: req.body });
 });
+// tests for auth middleware routes
+// router.get('/set-token-cookie', asyncHandler(async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-duck'
+//       }
+// });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// }));
+
+// router.get(
+//       '/restore-user',
+//       restoreUser,
+//       (req, res) => {
+//         return res.json(req.user);
+//       }
+// );
+
+// router.get(
+//       '/require-auth',
+//       requireAuth,
+//       (req, res) => {
+//         return res.json(req.user);
+//       }
+//     );
+
 
 module.exports = router;
