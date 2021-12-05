67618b1686ec63e60d357fc8332b028cfca27c71
andyrose507 <drew.jones1998@gmail.com>
2021-12-05 15:19:28 -0700

    login form display and redirection works

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,36 @@
+import React, { useState, useEffect } from 'react';
+import { Route, Switch } from 'react-router-dom';
+import LoginFormPage from './components/LoginFormPage';
+import { useDispatch, useSelector } from "react-redux";
+
+import * as sessionActions from "./store/session";
+
 function App() {
-  return (
-    <h1>Hello from App</h1>
+  const dispatch = useDispatch();
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const user = useSelector(state => {
+    if (state.session.user != null) {
+      return state.session.user.username
+    } else return 'no user session recognized'
+  })
+
+  useEffect(() => {
+    dispatch(sessionActions.restoreUser()).then(() => setIsLoaded(true));
+  }, [dispatch]);
+
+  return isLoaded && (
+    <Switch>
+      <Route path='/login'>
+        <LoginFormPage />
+      </Route>
+      <Route path='/' exact>
+        <>
+          <div>Splash Page</div>
+          <div>User: {user}</div>
+        </>
+      </Route>
+    </Switch>
   );
 }
 
